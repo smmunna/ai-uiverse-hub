92dd03f16f1f83afe82b4a4b452379a8befed728
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-02 22:19:04 +0600

    Inserted data to the divContainer

--- js/app.js
+++ js/app.js
@@ -36,8 +36,16 @@ const displayData = (data) =>{
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
         `
+        // Inserted to the divContainer
+        divContainer.appendChild(div);
     })
 
 
