5e4261509ef5c47d8837329c6e9cd81ea6045291
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-02 23:05:35 +0600

    Date Added

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ const displayData = (data) =>{
         // Destructuring the array;
         const {description,id,image,name,published_in,features,links} = aiHub;
        
+        console.log(features)
         // Create a div for inserting element to the divContainer;
         const div = document.createElement('div');
         div.classList.add('col');
@@ -43,12 +44,29 @@ const displayData = (data) =>{
           <img src="${image ? image:'../images/error.gif' }" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Features</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div>
+                <ol id="listItem">
+                
+                </ol>
+            </div>
+            <hr>
+            <h3>${name}</h3>
+            <div class="d-flex gap-2 justify-content-start">
+                <div class="date-img">
+                    <img src="../images/date.png" />
+                </div>
+                <div class="publish-data">
+                    <p>${published_in}</p>
+                </div>
+                
+            </div>
           </div>
         </div>
         `
         // Inserted to the divContainer
         divContainer.appendChild(div);
+
+      
     })
 
 
